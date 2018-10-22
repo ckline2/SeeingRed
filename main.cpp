60edc874189f568bf7e1838ca2139d0a00bffbd6
ckline2 <ckline2@mail.csuchico.edu>
2018-10-22 16:21:01 -0700

    Seeing red part 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Bitmap/Bitmap.h"
+#include "Bitmap/bitmap.h"
 #include <iostream>
 #include <vector>
 
@@ -8,6 +8,29 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
-  return 0;
+        string filename;
+        Bitmap image;
+
+        if (argc != 2)
+        {
+                cout<<"Please specify one image file!\n";
+        }
+
+        if (argc == 2) 
+        {
+                filename = argv[1];
+                image.open(filename);
+        }
+
+        if (image.isImage() == false)
+        {
+                cout<<"Image file must be a bitmap with 24-bit color depth.\n ";
+        }
+
+        if (image.isImage() == true)
+        {
+                vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
+                cout<<filename<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" wide "<<endl;
+        }
+    return 0;
 }
