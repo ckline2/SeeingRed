4adc5cac7c7610d3ba1c5d07181e038ece235b59
ckline2 <ckline2@mail.csuchico.edu>
2018-10-23 19:51:10 -0700

    SeeingRed Part 2

--- main.cpp
+++ main.cpp
@@ -10,27 +10,48 @@ int main(int argc, char* argv[])
 {
         string filename;
         Bitmap image;
+        image.open(filename);
 
         if (argc != 2)
         {
                 cout<<"Please specify one image file!\n";
         }
 
-        if (argc == 2) 
+        else 
         {
-                filename = argv[1];
+                string filename = argv[1];
+                Bitmap image;
                 image.open(filename);
-        }
 
-        if (image.isImage() == false)
-        {
-                cout<<"Image file must be a bitmap with 24-bit color depth.\n ";
-        }
+                if(!(image.isImage()))
+                {
+                        cout<<"Image file must be a bitmap with 24-bit color depth.\n ";
+                }
 
-        if (image.isImage() == true)
-        {
-                vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
-                cout<<filename<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" wide "<<endl;
-        }
-    return 0;
+                else
+                {
+                        vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
+                        cout<<argv[1]<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" pixels wide "<<endl;
+                
+
+                for (int row = 0;row < imagePixels.size(); row++)
+                {
+                        for(int column = 0;column < imagePixels[0].size();column++)
+                        {
+                                Pixel color;
+
+                                color = imagePixels[row][column];
+                                color.green = 0;
+
+                                color.blue = 0;
+                                imagePixels[row][column] = color;
+                        } 
+                }
+                image.fromPixelMatrix(imagePixels);
+                image.save("redness.bmp");
+        
+                }
+        }        
+                  
+        return 0;
 }
